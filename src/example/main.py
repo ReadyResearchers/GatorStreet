ff6a09c54c1cce0e232bd72406ee8860e3dc90c2
cheng2 <cheng2@allegheny.edu>
2022-10-05 13:06:02 -0400

    code: adding something to the main.py file

--- src/example/main.py
+++ src/example/main.py
@@ -5,14 +5,11 @@ from datetime import date # https://docs.python.org/3/library/datetime.html
 
 import yfinance as yf # https://pypi.org/project/yfinance/
 
+# pytest (203) 
+
 from prophet import Prophet
 from prophet.plot import plot_plotly
 from plotly import graph_objs as go 
-
-# scikit - learn imports
-# streamlit run (file) 
-# to run the app
-# https://scikit-learn.org/stable/
 
 START = "2017-01-01"
 
@@ -21,7 +18,11 @@ Today = date.today().strftime("%Y-%m-%d") # note its a set format, looking up ye
 st.title("STOCKS app")
 
 # make an input str here to take in stock names
-# print("enter stock")
+# print("enter stock") TODO? 
+# current bug 
+# enter GOOG => [G,O,O,G], could be all lower case
+# change it from lowercase to cap
+# return a str 
 stocks = ("GOOG","AAPL","ASFT","GME")
 selected_stock = st.selectbox("Select Stock", stocks)
 
@@ -29,8 +30,8 @@ selected_stock = st.selectbox("Select Stock", stocks)
 x_years = st.slider("Select days for prediction (working progress)" , 1, 4) # 1 - 6 days into the future
 # could use in days (most up to date)
 period = x_years * 365 # could have some math problem here 
-# TODO using what period?
-# TODO testing using (years)
+#  using what period?
+#  testing using (years)
 
 
 # loading dataset here and graph 
@@ -47,8 +48,8 @@ data_loaded = st.text("Loading data") # starting with this
 data = loading(selected_stock)
 # making yearly prediction here  :
 
-st.subheader("raw")
-st.write(data.tail())
+# st.subheader("raw")
+# st.write(data.tail())
 
 # plotting 
 def plot_r_data():
@@ -58,6 +59,9 @@ def plot_r_data():
     figure.layout.update(title="time series testing data", xaxis_rangeslider_visible=True)
     st.plotly_chart(figure)
 plot_r_data()
+
+
+# make an prediction model here
 
 data_train = data[["Date","Close"]]
 data_train = data_train.rename(columns={"Date": "ds", "Close": "y"})
@@ -69,14 +73,10 @@ future_data = m.make_future_dataframe(periods = period) # in years
 prediction = m.predict(future_data)
 
 
-st.subheader("Future data")
-st.write(prediction.tail())
+# st.subheader("Future data")
+# st.write(prediction.tail())
 
-st.write('making prediction')
+st.write('Making prediction using raw data as input: ')
 fig1 = plot_plotly(m, prediction)
 st.plotly_chart(fig1)
 
-
-# TODO make an prediction model here
-# TODO find a package that allows python to predict X with a vertain amount of time
-# similar to prophet but different (prophet not installing)
